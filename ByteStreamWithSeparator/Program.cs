879d736334e5800b13351d5a1568c0b1f923e568
Ярослав Горегляд <yaroslav.goreglyad123@gmail.com>
2024-03-03 21:56:35 +0300

    Add project files.

---
+++ ByteStreamWithSeparator/Program.cs
@@ -0,0 +1,12 @@
+﻿using ByteStreamWithSeparator;
+
+IStreamWithSeparatorService streamWithSeparatorService = new StreamWithSeparatorService();
+
+var path = Console.ReadLine();
+
+using (FileStream fs = File.OpenRead(path))
+{
+    byte[] buffer = new byte[fs.Length];
+    await fs.ReadAsync(buffer, 0, buffer.Length);
+    var messages = streamWithSeparatorService.Separate(buffer, (byte)';');
+}
---
+++ ByteStreamWithSeparator/StreamWithSeparatorService.cs
@@ -0,0 +1,44 @@
+﻿namespace ByteStreamWithSeparator
+{
+    public interface IStreamWithSeparatorService
+    {
+        List<string> Separate(byte[] source, byte separator);
+    }
+
+    public class StreamWithSeparatorService : IStreamWithSeparatorService
+    {
+        public List<string> Separate(byte[] source, byte separator)
+        {
+            var results = new List<string>();
+            var index = 0;
+
+            for (var i = 0; i < source.Length; i++)
+            {
+                if (source[i] == separator)
+                {
+                    if (i - index == 0)
+                    {
+                        index = i + 1;
+                        continue;
+                    }
+
+                    var partOfSource = new byte[i - index];
+                    
+                    Array.Copy(source, index, partOfSource, 0, partOfSource.Length);
+                    results.Add(System.Text.Encoding.Default.GetString(partOfSource));
+                    index = i + 1;
+                }
+            }
+
+            var finalPartOfSource = new byte[source.Length - index];
+
+            if (finalPartOfSource.Length == 0)
+                return results;
+
+            Array.Copy(source, index, finalPartOfSource, 0, finalPartOfSource.Length);
+            results.Add(System.Text.Encoding.Default.GetString(finalPartOfSource));
+
+            return results;
+        }
+    }
+}
